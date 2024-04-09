8c40d2bafb837937da15dd8f36dab05cba1c73a8
Sidhart Singh <isidhartsingh@gmail.com>
2024-04-09 14:06:50 +0530

    Initial Commit

---
+++ src/main/java/com/sidhartsingh/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.sidhartsingh.controller;
+
+import com.sidhartsingh.dto.AuthRequest;
+import com.sidhartsingh.dto.Product;
+import com.sidhartsingh.entity.UserInfo;
+import com.sidhartsingh.service.JwtService;
+import com.sidhartsingh.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+
+
+
+}
---
+++ src/main/java/com/sidhartsingh/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.sidhartsingh.repository;
+
+import com.sidhartsingh.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
